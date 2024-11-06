4bd7a9fe9c905a5278a6c95c462160976abebaf0
Ravindu Nethsara <139568202+NethsaraWMRC@users.noreply.github.com>
2024-11-07 00:21:42 +0530

    order files added

---
+++ order/src/main/java/com/kaleidoscope/order/service/shippingService.java
@@ -0,0 +1,50 @@
+package com.kaleidoscope.order.service;
+
+import com.kaleidoscope.order.dto.paymentDto;
+import com.kaleidoscope.order.model.paymentModel;
+import com.kaleidoscope.order.repo.shippingRepo;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.kaleidoscope.order.dto.shippingDto;
+import com.kaleidoscope.order.model.shippingModel;
+import java.util.List;
+
+@Service
+@Transactional
+
+public class shippingService {
+    @Autowired
+    private shippingRepo shippingRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<shippingDto> getAllShipping(){
+        List<shippingModel> shippingList = shippingRepository.findAll();
+        return modelMapper.map(shippingList, new TypeToken<List<shippingDto>>(){}.getType());
+    }
+
+    public shippingDto addShipping(shippingDto shippingDto){
+        shippingRepository.save(modelMapper.map(shippingDto, shippingModel.class));
+        return shippingDto;
+    }
+
+    public shippingDto updateShipping(shippingDto shippingDto){
+        shippingRepository.save(modelMapper.map(shippingDto, shippingModel.class));
+        return shippingDto;
+    }
+
+    public String deleteShipping(Integer ItemId){
+//        shippingRepository.delete(modelMapper.map(shippingDto, shippingModel.class));
+        shippingRepository.deleteById(ItemId);
+        return "Shipping deleted";
+    }
+
+    public List<shippingDto> getShippingByOrderId(Integer orderId) {
+        List<shippingModel> shippingtList = shippingRepository.findByOrderId(orderId);
+        return modelMapper.map(shippingtList, new TypeToken<List<shippingDto>>() {}.getType());
+    }
+}
