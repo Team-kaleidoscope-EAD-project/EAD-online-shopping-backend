8c6becc66e9767e6841b3c361819a1156d8bcee0
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-11-20 00:59:56 +0530

    Configure to enable endpoints public

--- apigateway/src/main/java/com/kaleidoscope/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/com/kaleidoscope/apigateway/config/SecurityConfig.java
@@ -2,23 +2,26 @@ package com.kaleidoscope.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableWebFluxSecurity
+@Slf4j
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
-                        .permitAll()
-                        .anyExchange().authenticated()
-                ).oauth2ResourceServer((oauth) -> oauth
-                        .jwt(Customizer.withDefaults()))
-                .build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // Disable CSRF if not required
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/v1/apigateway/public", "/api/user/**").permitAll()
+
+                );
+
+        log.info("SecurityWebFilterChain configured to allow all endpoints publicly.");
+        return http.build();
     }
 }
