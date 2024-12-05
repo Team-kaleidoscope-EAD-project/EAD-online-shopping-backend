2122a2b3267f28aa90111734c5ab8395ebaa3dce
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-12-05 17:08:50 +0530

    Secure order service endpoints

--- apigateway/src/main/java/com/kaleidoscope/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/com/kaleidoscope/apigateway/config/SecurityConfig.java
@@ -33,33 +33,52 @@ public class SecurityConfig implements WebFluxConfigurer {
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable) // Disable CSRF if not required
                 .authorizeExchange(exchanges -> exchanges
+                                // Product service
                                 .pathMatchers("/api/v1/product/","/api/v1/product/test","/api/v1/product/{id}","/api/v1/product/sku/{sku}","/api/v1/product/filter").permitAll()
                                 .pathMatchers(HttpMethod.POST, "/api/v1/product/addproduct").hasRole("kalei_ADMIN")
                                 .pathMatchers(HttpMethod.PUT, "/api/v1/product/{id}").hasRole("kalei_ADMIN")
                                 .pathMatchers(HttpMethod.DELETE, "/api/v1/product/{id}").hasRole("kalei_ADMIN")
 
+                                // Feedback service
                                 .pathMatchers("/api/v1/feedback/getfeedbacks","/api/v1/feedback/getfeedback/byproduct/{productId}","/api/v1/feedback/getfeedback/byuser/{userId}").permitAll()
                                 .pathMatchers(HttpMethod.POST, "/api/v1/feedback/addfeedback").hasRole("kalei_CLIENT")
                                 .pathMatchers(HttpMethod.PUT, "/api/v1/feedback/updatefeedback/{feedbackId}").hasRole("kalei_CLIENT")
                                 .pathMatchers(HttpMethod.DELETE, "/api/v1/feedback/deletefeedback/{feedbackId}").hasRole("kalei_CLIENT")
 
+                                // Inventory service
                                 .pathMatchers("/api/v1/inventory/getinventoryitems","/api/v1/inventory/getinventoryitem/{sku}","/api/v1/inventory/test").permitAll()
                                 .pathMatchers(HttpMethod.POST, "/api/v1/inventory/addinventoryitem").hasRole("kalei_ADMIN")
                                 .pathMatchers(HttpMethod.PUT, "/api/v1/inventory/updateinventoryitem").hasRole("kalei_ADMIN")
                                 .pathMatchers(HttpMethod.DELETE, "/api/v1/inventory/deleteinventoryitem/{sku}").hasRole("kalei_ADMIN")
-//                        .pathMatchers("/api/v1/inventory/getinventoryitems").hasRole("kalei_CLIENT")
-//                                .pathMatchers("api/v1/feedback/public").hasRole("kalei_CLIENT")
-//                        .pathMatchers(AUTH_WHITELIST).permitAll()
-//                        .pathMatchers("/api/auth/**", "/api/**").permitAll()
-//                        .pathMatchers(HttpMethod.GET, "/api/v1/product", "/api/v1/product/{productId}", "/api/v1/product/categories", "/api/v1/product/search").permitAll()
-//                        .pathMatchers(HttpMethod.POST, "/api/v1/product").hasRole("ADMIN")
-//                        .pathMatchers(HttpMethod.PUT, "/api/v1/product/{productId}").hasRole("ADMIN")
-//                        .pathMatchers(HttpMethod.DELETE, "/api/v1/product/{productId}").hasRole("ADMIN")
-//                        .pathMatchers("/api/inventory/**").hasAnyRole("ADMIN")
-//                        .pathMatchers("/api/order/**").hasAnyRole("USER")
-//                                .pathMatchers("/api/v1/product/").hasRole("kalei_CLIENT")
-//                        .pathMatchers("/api/v1/order/getallorders").hasRole("kalei_CLIENT")
-                        .anyExchange().authenticated()
+
+                                // Order service
+                                // Order controller
+                                .pathMatchers("/api/v1/order/test","/api/v1/order/test","/api/v1/product/{id}","/api/v1/product/sku/{sku}","/api/v1/product/filter").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/publish","/api/v1/order/addorder").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updateOrder").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deleteorder/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/api/v1/order/getallorders").hasRole("kalei_ADMIN")
+                                // Order item controller
+                                .pathMatchers("/api/v1/order/getallorderitem","api/v1/order/getallorderitemsbyorderid/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addorderitems").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updateorderitem").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deletorderitem/{itemId}").permitAll()
+                                // Payment controller
+                                .pathMatchers("/api/v1/order/getallpayments","api/v1/order/getpaymentbyorderid/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addpayment").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updatepayment").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deletepayment/{paymentId}").permitAll()
+                                // Place order controller
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addplaceorder","/api/v1/order/confirmorder").permitAll()
+                                // Shipping controller
+                                .pathMatchers("/api/v1/order/getallshippings","api/v1/order/getshippingbyorderid/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addshipping").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updateshipping").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deleteshipping/{shippingId}").permitAll()
+                                // Stripe payment controller
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/stripepayment/create-payment-intent").permitAll()
+
+                                .anyExchange().authenticated()
                 )
 
                 .oauth2ResourceServer(oauth2 -> oauth2
--- order/src/main/java/com/kaleidoscope/order/config/SecurityConfig.java
+++ order/src/main/java/com/kaleidoscope/order/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.kaleidoscope.order.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -32,19 +33,39 @@ public class SecurityConfig implements WebFluxConfigurer {
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-//                                .pathMatchers("/api/v1/inventory/getinventoryitems").permitAll()
-//                        .pathMatchers(AUTH_WHITELIST).permitAll()
-                        .pathMatchers("/api/auth/**", "/api/**").permitAll()
-//                        .pathMatchers(HttpMethod.GET, "/api/v1/product", "/api/v1/product/{productId}", "/api/v1/product/categories", "/api/v1/product/search").permitAll()
-//                        .pathMatchers(HttpMethod.POST, "/api/v1/product").hasRole("ADMIN")
-//                        .pathMatchers(HttpMethod.PUT, "/api/v1/product/{productId}").hasRole("ADMIN")
-//                        .pathMatchers(HttpMethod.DELETE, "/api/v1/product/{productId}").hasRole("ADMIN")
-//                        .pathMatchers("/api/inventory/**").hasAnyRole("ADMIN")
-//                        .pathMatchers("/api/order/**").hasAnyRole("USER")
-//                                .pathMatchers("/api/v1/product/").hasRole("kalei_ADMIN")
-//                                .pathMatchers("/api/v1/order/getallorders").hasRole("kalei_CLIENT")
-//
-//                             .anyExchange().authenticated()
+                                // Order controller
+                                .pathMatchers("/api/v1/order/test","/api/v1/order/test","/api/v1/product/{id}","/api/v1/product/sku/{sku}","/api/v1/product/filter").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/publish","/api/v1/order/addorder").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updateOrder").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deleteorder/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.GET, "/api/v1/order/getallorders").hasRole("kalei_ADMIN")
+
+                                // Order item controller
+                                .pathMatchers("/api/v1/order/getallorderitem","api/v1/order/getallorderitemsbyorderid/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addorderitems").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updateorderitem").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deletorderitem/{itemId}").permitAll()
+
+                                // Payment controller
+                                .pathMatchers("/api/v1/order/getallpayments","api/v1/order/getpaymentbyorderid/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addpayment").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updatepayment").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deletepayment/{paymentId}").permitAll()
+
+                                // Place order controller
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addplaceorder","/api/v1/order/confirmorder").permitAll()
+
+                                // Shipping controller
+                                .pathMatchers("/api/v1/order/getallshippings","api/v1/order/getshippingbyorderid/{orderId}").permitAll()
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/addshipping").permitAll()
+                                .pathMatchers(HttpMethod.PUT, "/api/v1/order/updateshipping").permitAll()
+                                .pathMatchers(HttpMethod.DELETE, "/api/v1/order/deleteshipping/{shippingId}").permitAll()
+
+                                // Stripe payment controller
+                                .pathMatchers(HttpMethod.POST, "/api/v1/order/stripepayment/create-payment-intent").permitAll()
+
+
+                        .anyExchange().authenticated()
                 )
 
                 .oauth2ResourceServer(oauth2 -> oauth2
